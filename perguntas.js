e7dce3fe5f09062662bdfb3218b3d8eda2fb0030
Gabriela Soares de LIma <146189027+gabisoareslima06@users.noreply.github.com>
2024-10-21 12:20:26 -0300

    gabi

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programaçao',
+    'O que é Phython?',
+    'O Phython é uma linguagem de progremação'
+)
+
+criaCartao(
+    'Geografia',
+
+    'Qual a capital da França?'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglẽs?',
+'Oi em ingles é Hi (RAI)'
+)
